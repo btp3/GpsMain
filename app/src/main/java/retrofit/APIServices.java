544ca17cf3d57f374d7a94368e35ed358ce46655
Iron_Man <archit2693.aj@gmail.com>
2017-06-25 00:07:52 +0530

    GPS Location Test

---
+++ app/src/main/java/retrofit/APIServices.java
@@ -0,0 +1,18 @@
+package retrofit;
+
+/**
+ * Created by Iron_Man on 24/06/17.
+ */
+
+import models.LocationModel;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface APIServices {
+
+    @POST("/api/create/")
+    Call<LocationModel> savePost(@Body LocationModel locationModel);
+}
