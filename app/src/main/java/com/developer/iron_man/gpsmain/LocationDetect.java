8ebc29806a573ebfe3fbd8c0e516bd1af76eb98e
Iron_Man <archit2693.aj@gmail.com>
2017-06-24 22:17:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/developer/iron_man/gpsmain/LocationDetect.java
@@ -0,0 +1,13 @@
+package com.developer.iron_man.gpsmain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationDetect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_detect);
+    }
+}
